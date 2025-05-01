c2237a5b57c9f8684ee08c422b84043e9aec03e6
Vitor <jvdeus27@gmail.com>
2025-05-01 15:55:40 -0300

    commit

--- backend/api/src/main/java/com/dinofestas/api/config/SecurityConfig.java
+++ backend/api/src/main/java/com/dinofestas/api/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://dino-festas.vercel.app/")); // Origem do Angular
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://dino-festas.vercel.app")); // Origem do Angular
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // Se estiver usando cookies/autenticação
