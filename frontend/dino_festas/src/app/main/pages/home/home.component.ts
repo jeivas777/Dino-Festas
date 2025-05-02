4cc05a0f9cc71c1eee609b15b78633ea08a5b4b0
Vitor <jvdeus27@gmail.com>
2025-05-01 22:55:13 -0300

    commit

--- frontend/dino_festas/src/app/main/pages/home/home.component.ts
+++ frontend/dino_festas/src/app/main/pages/home/home.component.ts
@@ -6,7 +6,7 @@ import { PacotesContainerComponent } from '../../components/pacotes-container/pa
   selector: 'app-home',
   imports: [ItensContainerComponent, PacotesContainerComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   constructor(private renderer: Renderer2, private el: ElementRef) {}
@@ -15,10 +15,16 @@ export class HomeComponent {
     const imgContainer = this.el.nativeElement.querySelector('.img-container');
     const bgImage = new Image();
     bgImage.src =
-      'https://imagens-kits-dinofestas.s3.us-east-2.amazonaws.com/AdobeStock_1308923584.jpeg';
+      'https://imagens-kits-dinofestas.s3.us-east-2.amazonaws.com/AdobeStock_1308923584.jpg'; // Use a URL com a extensão correta
 
     bgImage.onload = () => {
+      // A imagem foi carregada, agora a classe "loaded" será aplicada
       this.renderer.addClass(imgContainer, 'loaded');
+    };
+
+    // Caso haja erro ao carregar a imagem, podemos adicionar uma verificação
+    bgImage.onerror = () => {
+      console.error('Erro ao carregar a imagem de fundo');
     };
   }
 }
